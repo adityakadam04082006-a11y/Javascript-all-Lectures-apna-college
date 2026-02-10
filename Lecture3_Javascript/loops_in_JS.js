8358b1399bbebd28e600e52647fe5da34ce53eb9
ADITYA KADAM <adityakadam04082006@gmail.com>
2026-02-10 23:08:46 +0530

    Add files via upload

---
+++ Lecture3_Javascript/loops_in_JS.js
@@ -0,0 +1,14 @@
+// LOOPS in JS
+
+// loops are used to execute a piece of code again & again 
+// inside loops we write 3 conditions -(initilize , stoping condition , updation) this is 
+// condition 
+/* working -1) initilization see first condition works for one time
+2) Then the stoping condition works works like if True then checks isit true and goes
+further to check updation condition and goes back to check stoping condition and 
+works till the condition is satisfied or true ,see through example
+*/
+
+for (let count = 1 ; count <= 5 ; count ++) {
+    console.log("Aditya kadam")
+}
