ebe02cf886f09112057da0969bad4b417e7f4d68
ADITYA KADAM <adityakadam04082006@gmail.com>
2026-02-11 23:45:34 +0530

    Add files via upload

---
+++ Lecture3_Javascript/strings _in_JS.js
@@ -0,0 +1,11 @@
+let name = "Adi";
+let city = 'Pune';
+let str = "JavaScript";
+console.log(str.length);   // 10
+let str = "Hello";
+console.log(str[0]);   // H
+console.log(str[4]);   // o
+let first = "Hello";
+let second = "World";
+
+console.log(first + " " + second);   // Hello World
