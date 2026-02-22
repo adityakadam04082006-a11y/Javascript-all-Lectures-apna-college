0d1d7da7c46de56e9500771e8627257cfbf29ada
ADITYA KADAM <adityakadam04082006@gmail.com>
2026-02-23 00:29:20 +0530

    Update loops_in_JS.js

--- Lecture3_Javascript/loops_in_JS.js
+++ Lecture3_Javascript/loops_in_JS.js
@@ -22,3 +22,57 @@ for (let i = 1; i <= n; i++){
 }
 console.log("sum =", sum);
 console.log("loop ended")
+
+
+// Infinite loops in js :loops that never ends
+
+// ---------------While Loops-------------------
+
+/* while(condition){
+    your work in this
+}
+
+*/
+// print 1 to 5
+let j = 1;
+while (j <= 5){
+console.log("j =",j)
+j ++
+}
+
+/*-------------- do-while loop--------------------------
+see in this kind of loop we have a garuanty that the loop will run atleast once
+means see this */
+let k = 20;
+do {
+   console.log("adi kadam")
+   k++
+} while (k <= 10);
+
+/* see here the loop is partially not correct when you see the condition and the starting
+,now see this is ex to print from 1 to 10*/ 
+
+let h = 1;
+do {
+  console.log("h =",h)
+  h++
+} while (h <= 10);
+
+
+/*---------------for-of Loop----------------------------------
+this type of loop helps us to apply loop on string and arrays 
+see its working --
+
+let str = "zeal college"
+
+foe(let val of strVar){
+// do some work
+}
+
+see this is the way to write for-of loop*/
+
+let string ="Zeal college"
+
+for(let a of string){
+console.log("a =",a);
+}
